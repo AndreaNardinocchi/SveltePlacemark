616c5e62a59fdab890eaf825a1347ff3dd86ab6a
Andrea Nardinocchi <andrea.nardinocchi76@gmail.com>
2025-05-12 20:26:57 +0100

    Whatershade moment 2

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
 
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
+      //  "/api": "http://localhost:3000"
+      "/api": "https://placemark-v63d.onrender.com/"
     }
   }
 });
